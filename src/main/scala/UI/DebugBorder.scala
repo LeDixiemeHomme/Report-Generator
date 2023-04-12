0e72523a0b89529a7b7d61febd153e6a0d0e7420
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-04-12 17:09:52 +0200

    feat(debug-border): make debug border to track box area

---
+++ src/main/scala/UI/DebugBorder.scala
@@ -0,0 +1,13 @@
+package fr.valle.report_generator
+package UI
+
+import scalafx.scene.layout.{Border, BorderStroke, BorderStrokeStyle, BorderWidths}
+import scalafx.scene.paint.Color
+
+class DebugBorder(borderColor : Color) {
+  val border: Border = new Border(new BorderStroke(borderColor, BorderStrokeStyle.Solid, null, new BorderWidths(2)))
+}
+
+object DebugBorder {
+  def apply(borderColor: Color): DebugBorder = new DebugBorder(borderColor)
+}
