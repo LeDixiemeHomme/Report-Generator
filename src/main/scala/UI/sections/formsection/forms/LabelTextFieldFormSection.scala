9092eee125e3f709e72fafc173ad1e5ad1552759
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-04-13 22:52:03 +0200

    feat(file-name-field): add a class that create a label field without a browse button

---
+++ src/main/scala/UI/sections/formsection/forms/LabelTextFieldFormSection.scala
@@ -0,0 +1,33 @@
+package fr.valle.report_generator
+package UI.sections.formsection.forms
+
+import UI.DebugBorder
+import UI.sections.formsection.FormSectionTrait
+
+import scalafx.geometry.Pos
+import scalafx.scene.control.{Label, TextField}
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color
+
+class LabelTextFieldFormSection(label: String, val myTextField: TextField, required: Boolean) extends FormSectionTrait {
+  myTextField.prefWidth = 200
+
+  private val myLabel = new Label {
+    text = label
+    prefWidth = 110
+    alignment = Pos.CenterLeft
+    style = "-fx-text-fill: white;"
+  }
+  override def myFormSection: HBox = new HBox {
+    border = DebugBorder(Color.Red).border
+    prefWidth = 400
+    spacing = 20
+    children = Seq(myLabel, myTextField)
+  }
+
+  override def isRequired: Boolean = required
+}
+
+object LabelTextFieldFormSection {
+  def apply(label: String, myTextField: TextField, required: Boolean): LabelTextFieldFormSection = new LabelTextFieldFormSection(label, myTextField, required)
+}
