01b6bf746cdd1c0749bd9ef36e3e474c7d90e407
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-04-15 23:06:59 +0200

    feat(refacto-form-section): implement refacto

--- src/main/scala/UI/sections/pagesection/pagecontent/form/formsections/LabelTextFieldBrowseFormSection.scala
+++ src/main/scala/UI/sections/pagesection/pagecontent/form/formsections/LabelTextFieldBrowseFormSection.scala
@@ -11,16 +11,20 @@ import scalafx.scene.layout._
 import scalafx.scene.paint.Color
 
 class LabelTextFieldBrowseFormSection(label: String, val myTextField: TextField, required: Boolean, browseStrategy: BrowseButtonStrategyTrait) extends FormSectionTrait {
-  myTextField.prefWidth = 200
+  myTextField.prefWidth = 500
+
+  private val requiredString = if (required) " (*)" else ""
+
+  private val finalLabel: String = label + requiredString
 
   private val myLabel = new Label {
-    text = label
-    prefWidth = 110
+    text = finalLabel
+    prefWidth = 200
     alignment = Pos.CenterLeft
     style = "-fx-text-fill: white;"
   }
 
-  private val browseButton = browseStrategy.browseButton(myTextField, stage)
+  private val browseButton = browseStrategy.optionalBrowseButton(myTextField, stage).getOrElse(new HBox())
 
   val myFormSection: HBox = new HBox {
     border = DebugBorder(Color.Red).border
