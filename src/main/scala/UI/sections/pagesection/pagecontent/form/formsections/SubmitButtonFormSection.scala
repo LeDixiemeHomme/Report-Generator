c732373bc659ada27152112a254eda988d4acc59
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-04-15 23:08:15 +0200

    feat(rework-submit-button): centered, size, font size, in hbox

--- src/main/scala/UI/sections/pagesection/pagecontent/form/formsections/SubmitButtonFormSection.scala
+++ src/main/scala/UI/sections/pagesection/pagecontent/form/formsections/SubmitButtonFormSection.scala
@@ -1,15 +1,28 @@
 package fr.valle.report_generator
 package UI.sections.pagesection.pagecontent.form.formsections
 
+import UI.DebugBorder
+
 import scalafx.geometry.Pos
 import scalafx.scene.control.Button
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color
 
 class SubmitButtonFormSection() {
   //todo rework the button, text and position
+
   val myButton: Button = new Button {
-    alignment = Pos.Center
-    text = "Click me"
+    text = "Créer le rapport"
+    style = "-fx-font-size: 20px;"
     disable = true
+    prefHeight = 50
+    prefWidth = 200
+  }
+
+  val myButtonSection: HBox = new HBox {
+    border = DebugBorder(Color.Yellow).border
+    alignment = Pos.Center
+    children = Seq(myButton)
   }
 }
 
