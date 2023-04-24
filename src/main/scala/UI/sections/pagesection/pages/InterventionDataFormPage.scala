79eeba3f96eb10309053529909fa7b497ba4f685
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-04-24 21:05:46 +0200

    add debug option

--- src/main/scala/UI/sections/pagesection/pages/InterventionDataFormPage.scala
+++ src/main/scala/UI/sections/pagesection/pages/InterventionDataFormPage.scala
@@ -55,7 +55,7 @@ class InterventionDataFormPage extends Logging with IsAPageTrait {
     var templatePathTemp: String = templateFilePathFormSection.myTextField.getText
     var outputPathTemp: String = outputDirectoryFormSection.myTextField.getText
 
-    val debugMode: Boolean = true
+    val debugMode: Boolean = false
     if (debugMode) {
       dataPathTemp = "C:\\Users\\benoi\\Dev\\Projects\\Report-Generator\\src\\main\\resources\\inputs\\data\\intervention-data.csv"
       templatePathTemp = "C:\\Users\\benoi\\Dev\\Projects\\Report-Generator\\src\\main\\resources\\inputs\\templates\\AFC Delagrave avec balise.docx"
