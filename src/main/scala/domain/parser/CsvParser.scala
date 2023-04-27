c56bcee579ebe061c85c5c4e55215ab6d51eefd0
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-04-27 09:41:10 +0200

    feat(report-data-v1): semi colon delimiter

--- src/main/scala/domain/parser/CsvParser.scala
+++ src/main/scala/domain/parser/CsvParser.scala
@@ -3,7 +3,7 @@ package domain.parser
 
 import logging.LogsKeeper
 
-import com.github.tototoshi.csv.CSVReader
+import com.github.tototoshi.csv.{CSVReader, DefaultCSVFormat}
 import org.apache.logging.log4j.scala.Logging
 
 import java.io.File
@@ -12,6 +12,10 @@ object CsvParser extends Logging {
   def parseFile[A](filePath: String)(implicit parser: FileParserTrait[A]): List[A] = {
 
     LogsKeeper.keepAndLog(extLogger = logger, LogsKeeper.INFO, "Parsing csv " + filePath, classFrom = getClass)
+
+    implicit object MyFormat extends DefaultCSVFormat {
+      override val delimiter = ';'
+    }
 
     val inputFile = new File(filePath)
     val reader = CSVReader.open(inputFile)
