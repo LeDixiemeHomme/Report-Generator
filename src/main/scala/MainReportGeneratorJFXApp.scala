211580fdbb002b4d7e5b8c6b7363356836082e05
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-05-02 23:45:49 +0200

    feat(pre-release-refont): clear Main and create MainStageClass

--- src/main/scala/MainReportGeneratorJFXApp.scala
+++ src/main/scala/MainReportGeneratorJFXApp.scala
@@ -1,63 +1,17 @@
 package fr.valle.report_generator
 
-import UI.MainContent
-import UI.sections._
-import UI.sections.logssection.{IsALogsSectionTrait, LogsSection}
-import UI.sections.pagesection.pages.{IsAPageTrait, OtherReportFormPage, ReportDataV1FormPage}
-import UI.sections.pagesection.{IsAPageSectionTrait, PageSection}
-import UI.sections.titlesection.TitleSection
-import UI.sections.titlesection.titles.{IsATitleTrait, ReportGeneratorTitle}
+import UI.MainStage
 import logging.LogsKeeper
 
 import org.apache.logging.log4j.scala.Logging
 import scalafx.application.{JFXApp3, Platform}
-import scalafx.scene.Scene
-import scalafx.scene.paint.Color
-import scalafx.stage.Screen
 
 object MainReportGeneratorJFXApp extends Logging with JFXApp3 {
   Platform.startup(runnable = () => {})
 
-  private val appTitle: IsATitleTrait = ReportGeneratorTitle()
-
-  private val reportDataV1FormPage: IsAPageTrait = ReportDataV1FormPage()
-  private val otherReportFormPage: IsAPageTrait = OtherReportFormPage()
-
-  private val titleSection: IsASectionTrait = TitleSection(title = appTitle)
-
-  private val pageSection: IsAPageSectionTrait = PageSection(
-    pageList = reportDataV1FormPage :: otherReportFormPage :: Nil
-  )
-
-  private val navBarSection: IsASectionTrait = NavBarSection(pageSection = pageSection)
-
-  private val logsSection: IsALogsSectionTrait = LogsSection()
-
-  private val mainContent = MainContent(
-    listSection = List(titleSection, navBarSection, pageSection, logsSection)
-  ).myMainContent
-
   override def start(): Unit = {
-    LogsKeeper.keepAndLog(extLogger = logger, LogsKeeper.INFO, "Starting app", classFrom = getClass)
-    stage = new JFXApp3.PrimaryStage {
-      title = appTitle.toTitle
-      scene = new Scene {
-        fill = Color.rgb(38, 38, 38)
-        content = mainContent
-        stylesheets = List("style.css")
-      }
-    }
+    stage = MainStage().myStage
 
-    // center content when the window is maximized
-    stage.maximized.addListener { (_, _, isMaximized) =>
-      if (isMaximized) {
-        val bounds = Screen.primary.bounds
-        mainContent.translateX = (bounds.width - mainContent.width()) / 2
-        mainContent.translateY = (bounds.height - mainContent.height()) / 3.5
-      } else {
-        mainContent.translateX = 0
-        mainContent.translateY = 0
-      }
-    }
+    LogsKeeper.keepAndLog(extLogger = logger, LogsKeeper.INFO, "Starting app", classFrom = getClass)
   }
 }
---
+++ src/main/scala/UI/MainStage.scala
@@ -0,0 +1,76 @@
+package fr.valle.report_generator
+package UI
+
+import UI.MainStage.APP_TITLE
+import UI.sections._
+import UI.sections.logssection.{IsALogsSectionTrait, LogsSection}
+import UI.sections.pagesection.pages.{IsAPageTrait, OtherReportFormPage, ReportDataV1FormPage}
+import UI.sections.pagesection.{IsAPageSectionTrait, PageSection}
+import UI.sections.titlesection.TitleSection
+import UI.sections.titlesection.titles.{IsATitleTrait, ReportGeneratorTitle}
+
+import scalafx.application.JFXApp3
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.Scene
+import scalafx.scene.layout.{HBox, VBox}
+import scalafx.scene.paint.Color
+import scalafx.stage.Screen
+
+class MainStage {
+
+  private class StageContent() {
+    private val reportDataV1FormPage: IsAPageTrait = ReportDataV1FormPage()
+    private val otherReportFormPage: IsAPageTrait = OtherReportFormPage()
+
+    private val titleSection: IsASectionTrait = TitleSection(title = APP_TITLE)
+    private val pageSection: IsAPageSectionTrait = PageSection(pageList = reportDataV1FormPage :: otherReportFormPage :: Nil)
+    private val navBarSection: IsASectionTrait = NavBarSection(pageSection = pageSection)
+    private val logsSection: IsALogsSectionTrait = LogsSection()
+
+    private val listSection: List[IsASectionTrait] = List(titleSection, navBarSection, pageSection, logsSection)
+
+    private val contentVBox: VBox = new VBox {
+      border = DebugBorder(Color.Green).border
+      alignment = Pos.Center
+      padding = Insets(10)
+      prefWidth = 1600
+      children = listSection.map(_.mySection)
+    }
+
+    val myContent: HBox = new HBox {
+      border = DebugBorder(Color.Blue).border
+      alignment = Pos.Center
+      padding = Insets(5, 80, 5, 80)
+      prefWidth = 1600
+      children = contentVBox
+    }
+  }
+
+  private val mainContent = new StageContent().myContent
+
+  val myStage: JFXApp3.PrimaryStage = new JFXApp3.PrimaryStage {
+    title = APP_TITLE.toTitle
+    scene = new Scene {
+      fill = Color.rgb(38, 38, 38)
+      content = mainContent
+      stylesheets = List("style.css")
+    }
+    // center content when the window is maximized
+    maximized.addListener { (_, _, isMaximized) =>
+      if (isMaximized) {
+        val bounds = Screen.primary.bounds
+        mainContent.translateX = (bounds.width - mainContent.width()) / 2
+        mainContent.translateY = (bounds.height - mainContent.height()) / 3.5
+      } else {
+        mainContent.translateX = 0
+        mainContent.translateY = 0
+      }
+    }
+  }
+}
+
+object MainStage {
+  final val APP_TITLE: IsATitleTrait = ReportGeneratorTitle()
+
+  def apply(): MainStage = new MainStage()
+}
