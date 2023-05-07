75a52e99fc5719c51b76786fb2e1a6905aa09151
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-05-07 21:00:33 +0200

    feat(design-improving): textFields can be drag and drop

--- src/main/scala/UI/sections/pagesection/pagecontent/form/formsections/LabelTextFieldBrowseFormSection.scala
+++ src/main/scala/UI/sections/pagesection/pagecontent/form/formsections/LabelTextFieldBrowseFormSection.scala
@@ -5,13 +5,17 @@ import MainReportGeneratorJFXApp.stage
 import UI.DebugBorder
 import UI.sections.pagesection.pagecontent.form.formsections.browsebuttonstrategypattern.BrowseButtonStrategyTrait
 
+import javafx.scene.input.DragEvent
 import scalafx.geometry.Pos
 import scalafx.scene.control.{Label, TextField}
+import scalafx.scene.input.TransferMode
 import scalafx.scene.layout._
 import scalafx.scene.paint.Color
 
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
 class LabelTextFieldBrowseFormSection(finalLabel: String, example: String, required: Boolean,
-                                                browseStrategy: BrowseButtonStrategyTrait) extends IsAFormSectionTrait {
+                                      browseStrategy: BrowseButtonStrategyTrait) extends IsAFormSectionTrait {
 
   private val myLabel = new HBox {
     alignment = Pos.CenterLeft
@@ -31,6 +35,9 @@ class LabelTextFieldBrowseFormSection(finalLabel: String, example: String, requi
 
   private val browseButton = browseStrategy.optionalBrowseButton(textField, stage).getOrElse(new HBox())
 
+  browseButton.prefWidth = 200
+  browseButton.prefHeight = 30
+
   private val buttonHBox: HBox = new HBox {
     border = DebugBorder(Color.Red).border
     alignment = Pos.Center
@@ -45,13 +52,31 @@ class LabelTextFieldBrowseFormSection(finalLabel: String, example: String, requi
     children = Seq(myLabel, textField, buttonHBox)
   }
 
+  textField.onDragOver = (event: DragEvent) => {
+    if (event.getDragboard.hasFiles) {
+      event.acceptTransferModes(TransferMode.Copy)
+    } else {
+      event.consume()
+    }
+  }
+
+  textField.onDragDropped = (event: DragEvent) => {
+    val files = event.getDragboard.getFiles.asScala.toList
+    if (files.nonEmpty) {
+      val file = files.head
+      textField.setText(file.getPath)
+    }
+    event.consume()
+  }
+
   def myTextField: TextField = textField
 
   override def isRequired: Boolean = required
 }
 
 object LabelTextFieldBrowseFormSection {
-  def apply(label: String, example: String, required: Boolean, browseStrategy: BrowseButtonStrategyTrait): LabelTextFieldBrowseFormSection = {
+  def apply(label: String, example: String, required: Boolean,
+            browseStrategy: BrowseButtonStrategyTrait): LabelTextFieldBrowseFormSection = {
 
     val requiredString = if (required) " (*)" else ""
 
