bea4da09a69bb4fcf29992fcab043237196994d5
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-06-14 23:24:45 +0200

    fix OpenDocxReport by OS (windows now)

--- src/main/scala/features/services/OpenDocxReport.scala
+++ src/main/scala/features/services/OpenDocxReport.scala
@@ -1,14 +1,22 @@
 package fr.valle.report_generator
 package features.services
 
+import app.LocalOS
 import domain.path.FilePath
+import logging.{Levels, Log, LogsKeeper}
+
+import org.apache.logging.log4j.scala.Logging
 
 import scala.sys.process._
 
-class OpenDocxReport {
+class OpenDocxReport extends Logging {
   def open(fileLocation: String): Int = {
     val filePath: FilePath = FilePath.stringToFilePath(stringValue = fileLocation)
-    Seq("powershell", "/c", s"start '${filePath.constructBasePathAntiSlash}${filePath.fileName.value}.${filePath.extension}'").!
+
+    LocalOS.os match {
+      case LocalOS.OSs.WINDOWS => Seq("powershell", "/c", s"start '${filePath.constructBasePathAntiSlash}${filePath.fileName.value}.${filePath.extension}'").!
+      case _ => LogsKeeper.keepAndLog(extLogger = logger, log = Log(message = s"Not implemented yet for your operating system", level = Levels.ERROR), classFrom = getClass); 0
+    }
   }
 }
 
