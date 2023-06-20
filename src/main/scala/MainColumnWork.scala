29edd1a9cc83ba9fce54db32c11723b6e643db53
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2023-06-20 23:17:38 +0200

    feat(add-column): fix path Paths.get

--- src/main/scala/MainColumnWork.scala
+++ src/main/scala/MainColumnWork.scala
@@ -6,14 +6,16 @@ import domain.docx.reader.DocxReader
 import domain.docx.writer.DocxWriter
 import features.services.OpenDocxReport
 
+import java.nio.file.Paths
+
 object MainColumnWork extends App {
   val docxReader = DocxReader()
   val docxArrayModifier = DocxArrayModifier()
   val docxWriter = DocxWriter()
   val openDocxReport = OpenDocxReport()
 
-  val updatedDoc = docxArrayModifier.addColumn(document = docxReader.readDocx(templateFilePath = FilePath.stringToFilePath("C:\\Users\\U897R1\\Dev\\Projects\\Report-Generator\\src\\test\\resources\\template-test-add-column.docx")))
-  val writeResult = docxWriter.write(templateDoc = updatedDoc, FilePath.stringToFilePath("C:\\Users\\U897R1\\Dev\\Projects\\Report-Generator\\src\\test\\resources\\template-test-add-column-updated.docx"))
+  val updatedDoc = docxArrayModifier.addColumn(document = docxReader.readDocx(templateFilePath = FilePath.stringToFilePath(Paths.get("src/test/resources", "template-test-add-column.docx").toString)))
+  val writeResult = docxWriter.write(templateDoc = updatedDoc, FilePath.stringToFilePath(Paths.get("src/test/resources", "template-test-add-column-updated.docx").toString))
 
   println(writeResult)
   openDocxReport.open(fileLocation = writeResult.outputPath)
